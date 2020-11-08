10cee04ee9f9b61b9d873633b6a82299a94341cf
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-11-08 13:02:29 +0530

    Chat Application in Java

---
+++ Chat Application/clientsame.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+class clientsame{
+	public static void main(String[] args){
+		try{
+			Socket s = new Socket("localhost",1201);
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String msgin = "",msgout = "";
+
+			while(!msgin.equals("End")){
+				msgout = br.readLine();
+				dout.writeUTF(msgout);
+				msgin = din.readUTF();
+				System.out.println("Server says : "+msgin);
+			}
+
+		}catch(Exception e){
+			//handle exceptions here
+		}
+	}
+}
---
+++ Chat Application/serversame.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+class serversame{
+	public static void main(String[] args){
+		try{
+			ServerSocket ss = new ServerSocket(1201);
+			Socket s = ss.accept();
+
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String msgin = "",msgout = "";
+
+			while(!msgin.equals("End")){
+				msgin = din.readUTF();
+				System.out.println("Client says : "+msgin);
+				msgout = br.readLine();
+				dout.writeUTF(msgout);
+				dout.flush();
+			}
+			s.close();
+
+		}catch(Exception e){
+			//handle exceptions
+		}
+	}
+}
