52af3ff666fae4dae82c418116eb1d2f61473573
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-12-20 17:37:57 +0530

    Update AddTwoNum.java

--- AddTwoNum.java
+++ AddTwoNum.java
@@ -2,7 +2,7 @@ public class AddTwoNum{
 public static void main(String args[]){
 int num1=5,num2=10,sum;  
 sum=num1+num2;
-System.out.println("Sum is: "+sum);
+System.out.println("Sum is: "+sum); 
 }  
 }  
 
